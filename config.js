1d37eed946f3a191d60e1105d56e68082ffea601
Bellfroutcs <87676463+Bellfroutcs@users.noreply.github.com>
2022-11-06 12:07:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,35 +1,35 @@
 //=============『 Utama 』================== //
-global.owner = ['6287898307350'] 
-global.mods = [] 
-global.prems = [] 
+global.owner = ['6283816797646'] 
+global.mods = ['6283816797646'] 
+global.prems = ['6283816797646'] 
 
 //=============『 Info Owner 』============== //
-global.nameowner = 'Gempy⛧⁩⁩'
-global.numberowner = '6287898307350'
-global.instagram = 'https://www.instagram.com/gmpyyy'
-global.github = 'https://github.com/gmpyyy'
-global.dana = '087898307350'
-global.pulsa = '087898307350'
+global.nameowner = 'Ananda DEV⁩⁩'
+global.numberowner = '6283816797646'
+global.instagram = 'https://www.instagram.com/ananda.alif.rizky'
+global.github = 'https://github.com/Bellfroutcs'
+global.dana = '083816797646'
+global.pulsa = '083816797646'
 global.gopay = '-'
 
 //=============『 Info Bot 』=================//
-global.namebot = 'PENTABOT'
-global.gc = 'https://chat.whatsapp.com/ElBB5NrJS637TYqjJLHuk2'
-global.web = 'https://www.youtube.com/channel/UCZoVmApPxtLYgUWwBD4nbCw' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
-global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
+global.namebot = 'Shikimori BOTz'
+global.gc = '-'
+global.web = 'https://lynk.id/sewa/page/robot-wa' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
+global.price1 = 'https://lynk.id/sewa/page/robot-wa \n ```Check This Web```'
 
 //=======『 Tampilan Dan Lainnya 』============//
-global.fotonya1 = 'https://i.postimg.cc/MZjW7hb6/20220707-200605.jpg' //ganti jadi foto bot mu
-global.fotonya2 = 'https://i.postimg.cc/MZjW7hb6/20220707-200605.jpg' //ini juga ganti 
+global.fotonya1 = 'https://i.postimg.cc/cCcxD7v6/Shikimori-icon.jpg' //ganti jadi foto bot mu
+global.fotonya2 = 'https://i.postimg.cc/cCcxD7v6/Shikimori-icon.jpg' //ini juga ganti 
 global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
-global.wm = 'PENTABOT'
+global.wm = 'ShikiMori BOTz'
 global.watermark = wm
-global.wm2 = '                     「PENTABOT⁩ 」'
-global.wm3 = 'Gempy⛧'
+global.wm2 = '                     「 ShikiMori BOTz 」'
+global.wm3 = 'Ananda DEV'
 global.htki = '––––––『' 
 global.htka = '』––––––'
-global.media = 'https://i.postimg.cc/MZjW7hb6/20220707-200605.jpg'
+global.media = 'https://i.postimg.cc/cCcxD7v6/Shikimori-icon.jpg'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.wait = 'Process...'
 global.eror = 'ERROR'
@@ -37,7 +37,7 @@ global.benar = 'Benar ✅\n'
 global.salah = 'Salah ❌\n'
 global.stiker_wait = 'Stiker sedang dibuat'
 global.packname = 'Created by'
-global.author = 'Gempy⛧⁩⁩'
+global.author = 'ShikiMori BOTz⁩⁩'
 
 //=============『 Apikey 』================== //
 global.APIs = { // API Prefix
